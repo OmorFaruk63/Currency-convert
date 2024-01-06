c683cb588119d05f8d794154c8dbb97d0ffd52e0
OmorFaruk63 <omor3710@gmail.com>
2024-01-06 16:32:30 +0600

    add axios

--- src/CurrencyConverter/CurrencyConverter.jsx
+++ src/CurrencyConverter/CurrencyConverter.jsx
@@ -1,18 +1,19 @@
 import { useState } from "react";
 import "./CurrencyConverter.css";
-import data from "../Json/data.json";
+import Hook from "../hook/hook";
+
 const CurrencyConverter = () => {
-  const [currency, setCurrency] = useState(data);
-  const [amount, setAmount] = useState(0);
   const [from, setFrom] = useState("BDT");
+  const data = Hook(from.toLocaleLowerCase());
+  const [currency, setCurrency] = useState("");
+  const [amount, setAmount] = useState(0);
   const [to, setTo] = useState("USD");
-  const currencyArray = Object.keys(currency.results);
-  console.log(currency.results);
+  const currencyArray = data.bdt ? Object?.keys(data.bdt) : [];
   function convertCurrency() {}
   return (
     <div className="Currency-Converter">
       <h2>Currency Converter App</h2>
-      <h1>{}</h1>
+      <h1>{"$00"}</h1>
       <div>
         <label>
           Amount:
@@ -25,7 +26,7 @@ const CurrencyConverter = () => {
         <label>
           From Currency:
           <select value={from} onChange={(e) => setFrom(e.target.value)}>
-            {currencyArray.map((currency) => (
+            {currencyArray.slice(3, -1)?.map((currency) => (
               <option key={currency} value={currency}>
                 {currency}
               </option>
@@ -35,7 +36,7 @@ const CurrencyConverter = () => {
         <label>
           To Currency:
           <select value={to} onChange={(e) => setTo(e.target.value)}>
-            {currencyArray.map((currency) => (
+            {currencyArray?.slice(3, -1)?.map((currency) => (
               <option key={currency} value={currency}>
                 {currency}
               </option>
---
+++ src/hook/hook.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const Hook = (name = "bdt") => {
+  const [data, setData] = useState({});
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+  useEffect(() => {
+    axios(
+      `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${name}.json`
+    )
+      .then((res) => {
+        setData(res);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setError(err);
+        setLoading(false);
+      });
+  }, [name]);
+
+  return { data, error, loading };
+};
+
+export default Hook;
